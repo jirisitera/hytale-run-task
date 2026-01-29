a0762550efb5a375e37c05fb3afc2353fb46a622
jirisitera <78226565+jirisitera@users.noreply.github.com>
2026-01-29 23:56:07 +0100

    fix: more class definition up in execution order

--- plugin/src/main/java/com/japicraft/HytalePlugin.kt
+++ plugin/src/main/java/com/japicraft/HytalePlugin.kt
@@ -38,6 +38,7 @@ class HytalePlugin : Plugin<Project> {
             buildLocation.set(runExt.buildLocation)
             xmx.set(runExt.xmx)
             xms.set(runExt.xms)
+            mainClass.set("com.hypixel.hytale.Main")
             classpath = project.files(
                 project.layout.projectDirectory.dir(runPath.get()).file("Server/HytaleServer.jar").asFile.absolutePath
             )
